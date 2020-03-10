e41ee7c17a827b10e6ceae085b3b709e62bca763
Thayallan Srinathan <thayallan.srinathan@gmail.com>
2020-03-09 21:37:24 -0700

    method for getting time and generic methods

--- car.py
+++ car.py
@@ -28,11 +28,11 @@ class Car:
         time*.
         This does *not* account for the car's position within the acceleration range.
         It returns: 
-          the index of the range (or None if the car has passed out of all ranges, in which case it is assumed to have
-            zero acceleration)
-          the distance covered by the range
-          the speed the car starts the range with
-          the acceleration during the range
+         the index of the range (or None if the car has passed out of all ranges, in which case it is assumed to have
+           zero acceleration)
+         the distance covered by the range
+         the speed the car starts the range with
+         the acceleration during the range
           the time the car has spent in the range
         """
         # if the current time is less than the start time then return nothing
@@ -61,18 +61,46 @@ class Car:
                 i += 1
 
     def get_pos(self, time):
-      """
-      This function takes a time and returns the scalar value of the car along its rail at that time.
-      """
-      if time < self.start_time:
-          return 0
-      i, d, speed, a, time = self.get_interval(time)
-      if i is None:
-          scalar = self.accells[-1][0]
-      elif i > 0:
-          scalar = self.accells[i-1][0]
-      else:
-          scalar = 0
-      v2 = math.sqrt(speed ** 2 + 2 * a * d)
-      scalar += (speed + v2) / 2 * time
-      return scalar
+        """
+        This function takes a time and returns the scalar value of the car along its rail at that time.
+        """
+        if time < self.start_time:
+            return 0
+        i, d, speed, a, time = self.get_interval(time)
+        if i is None:
+            scalar = self.accells[-1][0]
+        elif i > 0:
+            scalar = self.accells[i-1][0]
+        else:
+            scalar = 0
+        v2 = math.sqrt(speed ** 2 + 2 * a * d)
+        scalar += (speed + v2) / 2 * time
+        return scalar
+    
+    def get_time(self):
+        """
+        This function returns the time at which the car has completed all of its acceleration ranges, and is 
+        assumed to be cruising at a constant velocity after leaving the intersection.
+        """
+        time = self.start_time
+        speed = self.start_speed
+        oldD = 0
+        for d, a in self.accells:
+            d -= oldD
+            v2 = math.sqrt(speed ** 2 + 2 * a * d)
+            t = 2 * d / (speed + v2)
+            time += t
+            speed = v2
+        return time
+    
+    def copy(self):
+        """
+        This copies a car
+        """
+        return Car(self.start_speed, self.rail, self.name, start_time=self.start_time, accells=self.accells.copy())
+    
+    def __repr__(self):
+        res = self.name + "[("
+        for d,a in self.accells:
+            res += str(round(d)) + "," + str(a) + ")("
+        return res + "]"
